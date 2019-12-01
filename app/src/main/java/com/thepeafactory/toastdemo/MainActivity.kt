2a3bf4a9c5aaa95447ffeab73b333606791f9792
Thomas Hosty Cundell <thomas@cundell.com>
2019-12-01 14:17:48 +0000

    Basic normal boring toast

--- app/src/main/java/com/thepeafactory/toastdemo/MainActivity.kt
+++ app/src/main/java/com/thepeafactory/toastdemo/MainActivity.kt
@@ -2,11 +2,17 @@ package com.thepeafactory.toastdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toast_button.setOnClickListener {
+            Toast.makeText(this, "Show Toast Clicked", Toast.LENGTH_LONG).show()
+        }
     }
 }
