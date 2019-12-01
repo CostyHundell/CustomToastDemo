4d9984b2e6afd4ecf030d22d7dd8099fe43db567
Thomas Hosty Cundell <thomas@cundell.com>
2019-12-01 15:02:50 +0000

    Add Gravity

--- app/src/main/java/com/thepeafactory/toastdemo/CustomToast.kt
+++ app/src/main/java/com/thepeafactory/toastdemo/CustomToast.kt
@@ -1,6 +1,7 @@
 package com.thepeafactory.toastdemo
 
 import android.content.Context
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.widget.TextView
 import android.widget.Toast
@@ -13,6 +14,7 @@ class CustomToast(context: Context): Toast(context) {
         view = LayoutInflater.from(context).inflate(R.layout.custom_toast_layout, null)
         toastTextView = view.findViewById(R.id.toast_text)
         duration = LENGTH_LONG
+        setGravity(Gravity.TOP, 0, 100)
     }
 
     fun setText(toastTextValue: String): CustomToast {
