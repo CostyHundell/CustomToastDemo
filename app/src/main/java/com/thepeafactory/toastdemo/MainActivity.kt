218338c4e5adde839089152f998fcdb4f1167390
Thomas Hosty Cundell <thomas@cundell.com>
2019-12-01 14:42:02 +0000

    Custom Toast to your liking

--- app/src/main/java/com/thepeafactory/toastdemo/MainActivity.kt
+++ app/src/main/java/com/thepeafactory/toastdemo/MainActivity.kt
@@ -2,8 +2,11 @@ package com.thepeafactory.toastdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.custom_toast_layout.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,8 +14,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val toast = Toast(this)
+        toast.view = LayoutInflater.from(this).inflate(R.layout.custom_toast_layout, null)
+        val toastTextView = toast.view.findViewById<TextView>(R.id.toast_text)
+        toastTextView.text = "Show Toast Clicked"
+        toast.duration = Toast.LENGTH_LONG
+
         toast_button.setOnClickListener {
-            Toast.makeText(this, "Show Toast Clicked", Toast.LENGTH_LONG).show()
+           toast.show()
         }
     }
 }
